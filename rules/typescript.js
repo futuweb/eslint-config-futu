93fb45cd8d86de26b52c8932006407a8d9b9b0db
amyli(李向维) <amyli@futunn.com>
2022-03-25 21:25:34 +0800

    feat: 添加ts规则

--- rules/typescript.js
+++ rules/typescript.js
@@ -10,5 +10,53 @@ module.exports = {
         '@typescript-eslint/no-explicit-any': ['warn'],
         // 是否可使用`@ts-<directive>`
         '@typescript-eslint/ban-ts-comment': ['warn'],
+        // 数组声明
+        '@typescript-eslint/array-type': ['error', 'array'],
+        // 对象类型声明使用interface
+        '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+        // 命名约定
+        '@typescript-eslint/naming-convention': [
+            'error',
+            {
+                'selector': 'interface',
+                'format': ['PascalCase'],
+                'custom': {
+                    'regex': '^[A-Z]',
+                    'match': true
+                }
+            }
+        ],
+        // 函数与类方法的返回值需要被显式的指定
+        '@typescript-eslint/explicit-module-boundary-types': ['error'],
+        // 方法签名的声明方式使用property格式
+        '@typescript-eslint/method-signature-style': ['error', 'property'],
+        // for循环
+        '@typescript-eslint/prefer-for-of': ['warn'],
+        // 使用 import type {} 进行类型的导入
+        '@typescript-eslint/consistent-type-imports': ['error', {'prefer': 'type-imports'}],
+        // 使用可选链
+        '@typescript-eslint/prefer-optional-chain': ['warn'],
+        // 不允许空的interface，使用了extends除外
+        '@typescript-eslint/no-empty-interface': ['error', {'allowSingleExtends': true}],
+        // 枚举成员必须初始化
+        '@typescript-eslint/prefer-enum-initializers': ['error'],
+        // 枚举成员值只能是string number boolean等，不允许需要计算的方式
+        '@typescript-eslint/prefer-literal-enum-member': ['error', { 'allowBitwiseExpressions': true }],
+        // 使用@ts-expect-error而不是@ts-ignore
+        '@typescript-eslint/prefer-ts-expect-error': ['warn'],
+        // 返回值是promise的函数必须声明async
+        '@typescript-eslint/promise-function-async': ['warn'],
+        // switch判断条是联合类型时，其每一个类型分支都需要被处理
+        '@typescript-eslint/switch-exhaustiveness-check': ['warn'],
+        // 类型定义 函数参数、对象属性、类成员 必须声明类型
+        '@typescript-eslint/typedef': [
+            'error',
+            {
+                'arrowParameter': true,
+                'parameter': true,
+                'propertyDeclaration': true,
+                'memberVariableDeclaration': true
+            }
+        ]
     }
 }
